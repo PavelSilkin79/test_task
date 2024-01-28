779a92c6bc2f05f428cee8f31dfe0bd53b79c9f7
pavel <pashasil-5@mail.ru>
2024-01-28 20:09:20 +0300

    modified:   cookbook/admin.py 	modified:   cookbook/models.py 	modified:   cookbook/views.py 	modified:   project/settings.py 	modified:   project/urls.py

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Product, Recipe, RecipeProduct
 
-# Register your models here.
+
+class RecipeProductInline(admin.TabularInline):
+    model = RecipeProduct
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    
+    pass
+    # list_display = ('name', 'cooked_times')  # Отображать эти поля в списке продуктов
+    # search_fields = ('name')  # Позволяет искать продукты по имени
+    # list_filter = ('cooked_times')  # Добавляет фильтры для количества приготовленных блюд
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeProductInline]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+
+
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Recipe, RecipeProduct, Product
+from django.db.models import Sum
 
-# Create your views here.
+
+def add_product_to_recipe(request):
+    recipe_id = request.GET.get('recipe_id')
+    product_id = request.GET.get('product_id')
+    weight = request.GET.get('weight')
+
+    recipe = Recipe.objects.get(id=recipe_id)
+    product = Product.objects.get(id=product_id)
+
+    recipe_product, created = RecipeProduct.objects.get_or_create(recipe=recipe, product=product)
+    recipe_product.weight = weight
+    recipe_product.save()
+
+    return JsonResponse({'success': True})
+
+def cook_recipe(request):
+    recipe_id = request.GET.get('recipe_id')
+
+    recipe = Recipe.objects.get(id=recipe_id)
+    for product in recipe.products.all():
+        product.cooked_times += 1
+        product.save()
+
+    return JsonResponse({'success': True})
+
+
+def show_recipes_without_product(request):
+    product_id = request.GET.get('product_id')
+
+    recipes = Recipe.objects.annotate(total_weight=Sum('recipeproduct__weight')) \
+                            .filter(recipeproduct__product_id=product_id, total_weight__lt=20)
+
+    context = {'recipes': recipes}
+
+    return render(request, 'cookbook/index.html', context)
